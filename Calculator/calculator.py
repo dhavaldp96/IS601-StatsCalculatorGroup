ee6d9a803f13018f30bb07ae4e2cbfdbeaa609ac
wenbo <iwoffee@gmail.com>
2020-11-06 15:29:58 -0500

    Fix: Check values for being valid numbers and not strings

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -12,6 +12,7 @@ from Calculator.mul import mul
 from Calculator.div import div
 from Calculator.square import square
 from Calculator.square_root import square_root
+from HelperFunctions.helper import Helper
 
 class Calculator:
     result = 0
@@ -20,25 +21,42 @@ class Calculator:
         pass
 
     def add(self, a, b):
+        if not Helper.check_number(a) or not Helper.check_number(b):
+            raise ValueError
         self.result = add(a, b)
         return self.result
 
     def subtract(self, a, b):
+        if not Helper.check_number(a) or not Helper.check_number(b):
+            raise ValueError
         self.result = sub(a, b)
         return self.result
 
     def multiply(self, a, b):
+        if not Helper.check_number(a) or not Helper.check_number(b):
+            raise ValueError
         self.result = mul(a, b)
         return self.result
 
     def divide(self, a, b):
+        if not Helper.check_number(a) or not Helper.check_number(b):
+            raise ValueError
         self.result = div(a, b)
         return self.result
 
     def square(self, a):
+        if not Helper.check_number(a):
+            raise ValueError
         self.result = square(a)
         return self.result
 
     def squareroot(self, a):
+        if not Helper.check_number(a):
+            raise ValueError
         self.result = square_root(a)
         return self.result
+
+if __name__ == '__main__':
+    cal = Calculator()
+    cal.add(1, "3")
+    print(cal.result)
